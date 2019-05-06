aa5ef7d89b00d9a7177a495cd7ab471773979b5b
Summer <summer.d.howard@gmail.com>
2019-05-06 10:00:14 -0500

    props work and modify state on start

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,13 +3,27 @@ import React, { Component } from 'react';
 export default class Clock extends Component {
     constructor(props) {
         super(props);
-        this.state = { date: 0 };
+        this.state = { date: props.date };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.date}</h1>
+                <h1>{this.state.date.toLocaleTimeString()}</h1>
             </div>
         );
     }
